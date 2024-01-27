c46aafdb015832a5519028a02cf6041efc3e1143
agusacost <juanagustinacost@gmail.com>
2024-01-27 18:57:28 -0300

    Se documenta el codigo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server)
 
+//io.on se ejecuta cuando usuario entra a servidor
+//socket.on escucha al evento chat
+//io.emit ejecuta al recibir el mensaje
 io.on('connection',(socket)=>{
     socket.on('chat',(msg)=>{
         io.emit('chat',msg)
